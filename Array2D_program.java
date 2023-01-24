7b6fb183acbafaac7244d5ca30fa3c22b7656602
Keerthanajella <123370962+Keerthanajella@users.noreply.github.com>
2023-01-24 14:55:55 +0530

    Add files via upload

---
+++ Array2D_program.java
@@ -0,0 +1,20 @@
+package JAVA_LOGICAL;
+
+public class Array2D_program
+{
+    public static void main(String[] args)
+    {
+        int a=2;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=6;j++)
+            {
+                System.out.print(a*j+" ");
+            }
+            a++;
+            System.out.println();
+        }
+
+    }
+
+}
